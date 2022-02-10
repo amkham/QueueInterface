2bb312f3fd850af3cf87cb58fdb9ac25242e7993
betir <betirov95@gmail.com>
2022-02-10 12:56:11 +0300

    Добавьте файлы проекта.

---
+++ QueueInterface/IQiueue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueInterface
+{
+    internal interface IQiueue
+    {
+
+        int Peek();
+
+        int Dequeue();
+
+        void Enqueue(int item);
+    }
+}
